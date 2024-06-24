812597f56f28af7365377034469c58d06d11b089
Samuel Ademujimi <samshotmedia01@gmail.com>
2024-06-24 10:50:03 +0100

    feat: use manrope text

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         home: const SplashView(),
         theme: ThemeData(
-          textTheme: GoogleFonts.soraTextTheme(),
+          textTheme: GoogleFonts.manropeTextTheme(),
         ),
       ),
     );
